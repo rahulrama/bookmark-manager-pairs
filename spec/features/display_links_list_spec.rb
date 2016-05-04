a475fc441b400b1c939b9ae65fa28c4290375d17
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-05-04 15:23:43 +0200

    Challenge 10: creating_a_modular_sinatra_app

--- spec/features/display_links_list_spec.rb
+++ spec/features/display_links_list_spec.rb
@@ -1,7 +1,8 @@
 feature 'display links list' do
   scenario 'displays a list of links' do
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tag: 'course material')
-    visit '/'
+    visit '/links'
+    expect(page.status_code).to eq 200
     within 'ul#links' do
       expect(page).to have_content('Makers Academy')
     end
