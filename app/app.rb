e99dc4b739007626130486bb7926de2b1b5746c1
letianw <tmac4ever0824@gmail.com>
2016-05-04 21:45:50 +1000

    closing #11 creating links

--- app/app.rb
+++ app/app.rb
@@ -7,9 +7,17 @@ class BookmarkManager < Sinatra::Base
   get '/links' do
     #what does this mean?
     @links = Link.all
-    erb :'/links/index'
+    erb :'links/index'
   end
 
+  get '/links/new' do
+    erb :'/links/new'
+  end
+
+  post '/links' do
+    Link.create(title: params[:title],url:params[:url])
+    redirect '/links'
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
